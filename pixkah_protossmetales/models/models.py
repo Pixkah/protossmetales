aaf05054a769bfbfe6810f83c47520017d645809
Alfredo Rodriguez <alfredo@pixkah.mx>
2017-12-11 03:06:16 -0600

    ticket auto populates quantity done

--- pixkah_protossmetales/models/models.py
+++ pixkah_protossmetales/models/models.py
@@ -32,11 +32,12 @@ class Scale(models.Model):
 class ScaleTicket(models.Model):
     _name = 'pixkah_protossmetales.scale_ticket'
 
-    scale_id = fields.Many2one('pixkah_protossmetales.scale', string="Scale")
-    picking_id = fields.Many2one('stock.picking', string='Transfer reference', index=True)
+    name = fields.Char(compute='_compute_name', string="Ticket")
 
-    name = fields.Char(compute='_compute_name')
-    folio = fields.Char(string="folio", required=True)
+    scale_id = fields.Many2one('pixkah_protossmetales.scale', string="Scale")
+    folio = fields.Char(string="Folio", required=True)
+
+    picking_id = fields.Many2one('stock.picking', string='Transfer reference', index=True)    
     vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle")
     first_date = fields.Date(string="Date")
     first_weight = fields.Float(string="Weight (kg)")
@@ -49,11 +50,19 @@ class ScaleTicket(models.Model):
       for record in self:
           record.net_weight = abs(record.second_weight - record.first_weight)
 
+    @api.onchange('first_weight', 'second_weight')
+    def _autocompute_net_weight(self):
+      self._compute_net_weight()
+
+    @api.depends('folio', 'scale_id')
+    def _autocompute_name(self):
+      self._compute_name()
+
     @api.multi
     def _compute_name(self):
       for record in self:
-        record.name = record.scale_id.short_name + '-' + record.folio
-
+          name = record.scale_id.short_name if record.scale_id else ""
+          record.name = name + '-' + record.folio if record.folio else ""
 
 class Picking(models.Model):
     _inherit = "stock.picking"
@@ -61,3 +70,10 @@ class Picking(models.Model):
     scale_tickets = fields.One2many('pixkah_protossmetales.scale_ticket', 'picking_id', string="Scale Tickets")
     scale_ticket_id = fields.Many2one('pixkah_protossmetales.scale_ticket', string="Scale Ticket")
     driver_id = fields.Many2one('hr.employee', string="Driver")
+
+    @api.onchange('scale_ticket_id')
+    def _autocompute_done(self):
+      for record in self:
+        if record.scale_ticket_id:
+          for move in record.move_lines:
+            move.quantity_done = record.scale_ticket_id.net_weight
