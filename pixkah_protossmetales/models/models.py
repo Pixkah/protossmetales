24cb8b73de2300b19606566ecd46d05dae371980
Alfredo Rodriguez <alfredo@pixkah.mx>
2018-01-05 02:09:24 -0600

    added fields and moved tickets away from inventory

--- pixkah_protossmetales/models/models.py
+++ pixkah_protossmetales/models/models.py
@@ -32,48 +32,52 @@ class Scale(models.Model):
 class ScaleTicket(models.Model):
     _name = 'pixkah_protossmetales.scale_ticket'
 
-    name = fields.Char(compute='_compute_name', string="Ticket")
+    name = fields.Char(compute='_compute_name', string="Ticket", store=True)
 
     scale_id = fields.Many2one('pixkah_protossmetales.scale', string="Scale")
-    folio = fields.Char(string="Folio", required=True)
+    folio = fields.Char(string="Folio", required=True, index=True)
 
-    picking_id = fields.Many2one('stock.picking', string='Transfer reference', index=True)    
+    driver_id = fields.Many2one('hr.employee', string="Driver")    
     vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle")
+    trailer_id = fields.Many2one('fleet.vehicle', string="Trailer")
     first_date = fields.Date(string="Date")
     first_weight = fields.Float(string="Weight (kg)")
     second_date = fields.Date(string="Date")
     second_weight = fields.Float(string="Weight (kg)")
-    net_weight = fields.Float(compute='_compute_net_weight')
+    net_weight = fields.Float(compute='_compute_net_weight', store=True)
 
-    @api.multi
+    @api.depends('first_weight', 'second_weight')
     def _compute_net_weight(self):
       for record in self:
           record.net_weight = abs(record.second_weight - record.first_weight)
 
-    @api.onchange('first_weight', 'second_weight')
-    def _autocompute_net_weight(self):
-      self._compute_net_weight()
-
     @api.depends('folio', 'scale_id')
-    def _autocompute_name(self):
-      self._compute_name()
-
-    @api.multi
     def _compute_name(self):
       for record in self:
           name = record.scale_id.short_name if record.scale_id else ""
           record.name = name + '-' + record.folio if record.folio else ""
 
+
+# Inventory
 class Picking(models.Model):
     _inherit = "stock.picking"
 
-    scale_tickets = fields.One2many('pixkah_protossmetales.scale_ticket', 'picking_id', string="Scale Tickets")
-    scale_ticket_id = fields.Many2one('pixkah_protossmetales.scale_ticket', string="Scale Ticket")
-    driver_id = fields.Many2one('hr.employee', string="Driver")
-
-    @api.onchange('scale_ticket_id')
+    @api.onchange('move_lines')
     def _autocompute_done(self):
       for record in self:
-        if record.scale_ticket_id:
-          for move in record.move_lines:
-            move.quantity_done = record.scale_ticket_id.net_weight
+        for move in record.move_lines:
+          if move.scale_ticket_id:
+            move.quantity_done = move.scale_ticket_id.net_weight
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    scale_ticket_id = fields.Many2one('pixkah_protossmetales.scale_ticket', string="Scale Ticket")
+
+'''
+    @api.depends('scale_ticket_id')
+    def _autocompute_quantity_done(self):
+      for move in self:
+        if move.scale_ticket_id:
+          move.quantity_done = move.scale_ticket_id.net_weight
+'''
